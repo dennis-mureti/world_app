a58bac6b83a13b2819a26fdc0995a24fd2982f28
Mutwoki Dennis Mureti <dennis.mutwoki@strathmore.edu>
2024-07-24 20:34:01 +0300

    latest

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,27 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    // to simulate a network request for s user name
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return 'Dennis';
-    });
-
-    String bio = await Future.delayed(const Duration(seconds: 2), () {
-      return 'Deveoper, musician and chicken keeper';
-    });
-
-    print('$username - $bio');
-  }
-
-  // int counter = 0;
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
